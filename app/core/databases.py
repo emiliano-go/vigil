1dff3e95dbdac56b18b27cbd57bd5d16a92b8b05
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-06-12 01:59:36 -0300

    Update databases.py

--- app/core/databases.py
+++ app/core/databases.py
@@ -6,9 +6,10 @@ from app.core.config import settings
 clickhouse = database_config(
     database_name="clickhouse",
     database_type="clickhouse",
-    database_url_sync=settings.clickhouse_native_url,
+    database_url_sync=settings.clickhouse_http_url,
     database_url_async=settings.clickhouse_async_url,
     default=True,
+    model_paths=["app/models"]
 )
 
 class Base(DeclarativeBase):
