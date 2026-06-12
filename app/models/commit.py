1084d0742eb12fe2da78ec66f865990833354b5a
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-06-11 22:34:40 -0300

    Create commit.py

---
+++ app/models/commit.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, DateTime, ForeignKey
+from datetime import datetime
+from dbwarden.schema import auto_schema
+from dbwarden import ChEngineSpec
+
+from app.core.databases import Base
+
+@auto_schema
+class Commit(Base):
+    __tablename__ = "commits"
+
+    repo : Mapped[str] = mapped_column(ForeignKey("repos.name"), primary_key=True, nullable=False)
+    sha : Mapped[str] = mapped_column(String, nullable=False)
+    author_login : Mapped[str] = mapped_column(String, nullable=False)
+    author_name : Mapped[str] = mapped_column(String, nullable=False)
+    author_email : Mapped[str] = mapped_column(String, nullable=False)
+    message : Mapped[str] = mapped_column(String, nullable=True)
+    committed_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+
+    class Meta:
+        ch_engine = ChEngineSpec("MergeTree")
+        ch_order_by = ["repo", "committed_at"]
+        ch_primary_key = "repo"
+        comment = "Commit event store"
